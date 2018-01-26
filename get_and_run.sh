e3e357d2fa3043c0a69b431792b5564181e8f108
david masclet <davidmasclet@gisgraphy.com>
2018-01-26 10:41:53 +0100

    fix name of docker image

--- get_and_run.sh
+++ get_and_run.sh
@@ -1,5 +1,4 @@
 #/bin/bash
 
 
-docker pull gisgraphy/gisgraphyofficial  && docker run -ti -p80:8080 --hostname=myhost.com gisgraphyofficial bash
-
+docker pull gisgraphy/gisgraphyofficial  && docker run -ti -p80:8080 --hostname=myhost.com gisgraphy/gisgraphyofficial bash
